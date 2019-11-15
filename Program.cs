69a4192b679188e5921c9b17e0483427dea2359e
Mark McCann <mjmccann12@me.com>
2019-11-15 13:59:21 -0600

    Practice Planet and Spaceships

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars", };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> planets = new List<string>()
+            {
+                "Uranus",
+                "Venus"
+            };
+
+            planetList.AddRange(planets);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(1, "Earth");
+            planetList.Add("Pluto");
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            planetList.Remove("Pluto");
+
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine($"this planet is: {planet}");
+            }
+        }
+    }
+}
